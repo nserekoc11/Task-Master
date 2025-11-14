87750155822866dad697eca9cebc3d008c87c251
nserekoc11 <85781633+nserekoc11@users.noreply.github.com>
2025-11-14 08:27:51 +0000

    initial commit

--- -1counter.py
+++ -1counter.py
@@ -1,8 +1,16 @@
 #-1counter program
-
 #input
 num = int(input("Enter any number to count down from: "))
+
+def countdown(n):
+    if n < 0:
+        print("Please enter a non-negative integer.")
+        return
+
 #countdown loop
-while num >= -1:
+while num >= 0:
     print(num)
     num -= 1
+    if num == 0:
+        print("Countdown finished!")
+        

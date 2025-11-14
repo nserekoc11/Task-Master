80b00f6ca440b2cdb77d03ea297d99a743b81e9e
nserekoc11 <85781633+nserekoc11@users.noreply.github.com>
2025-11-14 08:34:49 +0000

    initial commit

--- -1counter.py
+++ -1counter.py
@@ -1,16 +1,19 @@
 #-1counter program
+#Adding time for the countdown
+import time as t
+
 #input
 num = int(input("Enter any number to count down from: "))
 
 def countdown(n):
     if n < 0:
         print("Please enter a non-negative integer.")
-        return
+countdown(num)
 
 #countdown loop
 while num >= 0:
     print(num)
     num -= 1
-    if num == 0:
+    t.sleep(1)  # Pause for 1 second between counts
+    if num == -1:
         print("Countdown finished!")
-        
